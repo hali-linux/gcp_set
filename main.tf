0481e363362e83b7bd6fba6a4a28e77eb84e8f23
johnlee <test@example.com>
2022-07-05 15:25:24 +0900

    change1

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "google_compute_instance" "default" {
     metadata_startup_script = file("/root/gcp_set/script.txt")
 
     // Apply the firewall rule to allow external IPs to access this instance
-    tags = ["http-server"]
+    tags = ["http-server","ssh-server"]
 }
 
 resource "google_compute_firewall" "http-server" {
@@ -44,8 +44,18 @@ resource "google_compute_firewall" "http-server" {
     protocol = "tcp"
     ports    = ["80"]
   }
-
   // Allow traffic from everywhere to instances with an http-server tag
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["http-server"]
+}
+
+resource "google_compute_firewall" "ssh-server" {
+  name    = "default-allow-ssh-terraform"
+  network = "new-vpc"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh-server"]
 }
